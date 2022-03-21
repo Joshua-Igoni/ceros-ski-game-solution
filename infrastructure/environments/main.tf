0900e85c08251fbeee6fbc1fa26402e5d37c3b36
Josh bolten <jozhbolt@gmail.com>
2022-03-21 16:01:58 +0100

    optimized readability

--- infrastructure/environments/main.tf
+++ infrastructure/environments/main.tf
@@ -8,15 +8,15 @@
 * private network.
 */
 resource "aws_vpc" "main_vpc" {
-  cidr_block = "172.0.0.0/16"
-  enable_dns_support = true
-  enable_dns_hostnames = true
+  cidr_block            = "172.0.0.0/16"
+  enable_dns_support    = true
+  enable_dns_hostnames  = true
 
   tags = {
-    Application = "ceros-ski" 
-    Environment = var.environment
-    Name = "ceros-ski-${var.environment}-main_vpc"
-    Resource = "modules.environment.aws_vpc.main_vpc"
+    Application         = "ceros-ski" 
+    Environment         = var.environment
+    Name                = "ceros-ski-${var.environment}-main_vpc"
+    Resource            = "modules.environment.aws_vpc.main_vpc"
   }
 }
 
@@ -26,13 +26,13 @@ resource "aws_vpc" "main_vpc" {
 * addresses.
 */
 resource "aws_internet_gateway" "main_internet_gateway" {
-  vpc_id = aws_vpc.main_vpc.id
+  vpc_id        = aws_vpc.main_vpc.id
 
   tags = {
     Application = "ceros-ski"
     Environment = var.environment
-    Name = "ceros-ski-${var.environment}-main_internet_gateway"
-    Resource = "modules.environment.aws_internet_gateway.main_internet_gateway"
+    Name        = "ceros-ski-${var.environment}-main_internet_gateway"
+    Resource    = "modules.environment.aws_internet_gateway.main_internet_gateway"
   }
 }
 
@@ -50,8 +50,8 @@ resource "aws_eip" "eip_for_the_nat_gateway" {
   tags = {
     Application = "ceros-ski" 
     Environment = var.environment 
-    Name = "ceros-ski-${var.environment}-us-east-1a-eip_for_the_nat_gateway"
-    Resource = "modules.availability_zone.aws_eip.eip_for_the_nat_gateway"
+    Name        = "ceros-ski-${var.environment}-us-east-1a-eip_for_the_nat_gateway"
+    Resource    = "modules.availability_zone.aws_eip.eip_for_the_nat_gateway"
   }
 }
 
@@ -66,16 +66,16 @@ resource "aws_eip" "eip_for_the_nat_gateway" {
 * to go through, and aren't shielded by, the NAT Gateway.
 */
 resource "aws_subnet" "public_subnets" {
-    vpc_id = aws_vpc.main_vpc.id
-    cidr_block = cidrsubnet(aws_vpc.main_vpc.cidr_block, 8, 2 + count.index)
-    availability_zone = element(var.availability_zones, count.index)
+    vpc_id                  = aws_vpc.main_vpc.id
+    cidr_block              = cidrsubnet(aws_vpc.main_vpc.cidr_block, 8, 2 + count.index)
+    availability_zone       = element(var.availability_zones, count.index)
     map_public_ip_on_launch = true
-    count = var.public_subnets_count
+    count                   = var.public_subnets_count
     tags = {
-        Name = "ceros-ski-${count.index * 2 +1}.0_${element(var.availability_zones, count.index)}"
-        Application = "ceros-ski" 
-        Resource = "modules.availability_zone.aws_subnet.public_subnet"
-        Environment = var.environment
+        Name                = "ceros-ski-${count.index * 2 +1}.0_${element(var.availability_zones, count.index)}"
+        Application         = "ceros-ski" 
+        Resource            = "modules.availability_zone.aws_subnet.public_subnet"
+        Environment         = var.environment
  }
 }
 /**
@@ -86,13 +86,13 @@ resource "aws_subnet" "public_subnets" {
 
 resource "aws_nat_gateway" "nat_gateway" {
     allocation_id = aws_eip.eip_for_the_nat_gateway.id
-    subnet_id = element(aws_subnet.public_subnets.*.id, 0)
+    subnet_id     = element(aws_subnet.public_subnets.*.id, 0)
     
     tags = {
       Application = "ceros-ski" 
       Environment = var.environment 
-      Name = "ceros-ski-${var.environment}-us-east-1a"
-      Resource = "modules.availability_zone.aws_nat_gateway.nat_gateway"
+      Name        = "ceros-ski-${var.environment}-us-east-1a"
+      Resource    = "modules.availability_zone.aws_nat_gateway.nat_gateway"
  }
 }
 
@@ -100,13 +100,13 @@ resource "aws_nat_gateway" "nat_gateway" {
 * A route table for our public subnet.
 */
 resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.main_vpc.id 
+  vpc_id        = aws_vpc.main_vpc.id 
 
   tags = {
     Application = "ceros-ski" 
     Environment = var.environment 
-    Name = "ceros-ski-${var.environment}-us-east-1a-public"
-    Resource = "modules.availability_zone.aws_route_table.public_route_table"
+    Name        = "ceros-ski-${var.environment}-us-east-1a-public"
+    Resource    = "modules.availability_zone.aws_route_table.public_route_table"
   }
 }
 
@@ -115,7 +115,7 @@ resource "aws_route_table" "public_route_table" {
 * gateway.
 */
 resource "aws_route_table" "public_rt" {
-    vpc_id = aws_vpc.main_vpc.id
+    vpc_id         = aws_vpc.main_vpc.id
     route {
         cidr_block = "0.0.0.0/0"
         gateway_id = aws_internet_gateway.main_internet_gateway.id
@@ -123,8 +123,8 @@ resource "aws_route_table" "public_rt" {
 }
 
 resource "aws_route_table_association" "public" {
-    count = var.public_subnets_count
-    subnet_id = element(aws_subnet.public_subnets.*.id, count.index)
+    count          = var.public_subnets_count
+    subnet_id      = element(aws_subnet.public_subnets.*.id, count.index)
     route_table_id = aws_route_table.public_rt.id
 }
 
@@ -146,17 +146,17 @@ resource "aws_route_table_association" "public" {
 
 
 resource "aws_subnet" "private_subnets" {
-    vpc_id = aws_vpc.main_vpc.id
-    cidr_block = cidrsubnet(aws_vpc.main_vpc.cidr_block, 8, count.index)
-    availability_zone = element(var.availability_zones, count.index)
+    vpc_id                  = aws_vpc.main_vpc.id
+    cidr_block              = cidrsubnet(aws_vpc.main_vpc.cidr_block, 8, count.index)
+    availability_zone       = element(var.availability_zones, count.index)
     map_public_ip_on_launch = false
-    count = var.private_subnets_count
+    count                   = var.private_subnets_count
 
     tags = {
-        Name = "ceros-ski-${count.index *2}.0_${element(var.availability_zones, count.index)}"
-        Application = "ceros-ski" 
-        Environment = var.environment
-        Resource = "modules.availability_zone.aws_subnet.private_subnet"
+        Name                = "ceros-ski-${count.index *2}.0_${element(var.availability_zones, count.index)}"
+        Application         = "ceros-ski" 
+        Environment         = var.environment
+        Resource            = "modules.availability_zone.aws_subnet.private_subnet"
  }
 }
 
@@ -164,13 +164,13 @@ resource "aws_subnet" "private_subnets" {
 * A route table for the private subnet.
 */
 resource "aws_route_table" "private_route_table" {
-  vpc_id = aws_vpc.main_vpc.id 
+  vpc_id        = aws_vpc.main_vpc.id 
 
   tags = {
     Application = "ceros-ski" 
     Environment = var.environment 
-    Name = "ceros-ski-${var.environment}-us-east-1a-private"
-    Resource = "modules.availability_zone.aws_route_table.private_route_table"
+    Name        = "ceros-ski-${var.environment}-us-east-1a-private"
+    Resource    = "modules.availability_zone.aws_route_table.private_route_table"
   }
 }
 
@@ -180,16 +180,16 @@ resource "aws_route_table" "private_route_table" {
 */
 
 resource "aws_route_table" "private_rt" {
-    vpc_id = aws_vpc.main_vpc.id
+    vpc_id             = aws_vpc.main_vpc.id
     route {
-        cidr_block = "0.0.0.0/0"
+        cidr_block     = "0.0.0.0/0"
         nat_gateway_id = aws_nat_gateway.nat_gateway.id
  }
 }
 
 resource "aws_route_table_association" "private" {
-    count = var.private_subnets_count
-    subnet_id = element(aws_subnet.private_subnets.*.id, count.index)
+    count          = var.private_subnets_count
+    subnet_id      = element(aws_subnet.private_subnets.*.id, count.index)
     route_table_id = aws_route_table.private_rt.id
 }
 
@@ -205,27 +205,27 @@ resource "aws_route_table_association" "private" {
 * A security group to allow SSH access into our bastion instance.
 */
 resource "aws_security_group" "bastion" {
-  name = "bastion-security-group"
-  vpc_id = aws_vpc.main_vpc.id 
+  name          = "bastion-security-group"
+  vpc_id        = aws_vpc.main_vpc.id 
 
   ingress {
-    protocol = "tcp"
-    from_port = 22
-    to_port = 22
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
     cidr_blocks = [ "0.0.0.0/0" ]
   }
 
   egress {
-    protocol = -1
-    from_port = 0
-    to_port = 0
+    protocol    = -1
+    from_port   = 0
+    to_port     = 0
     cidr_blocks = [ "0.0.0.0/0" ]
   }
 
   tags = {
     Application = "ceros-ski" 
     Environment = var.environment 
-    Resource = "modules.availability_zone.aws_security_group.bastion"
+    Resource    = "modules.availability_zone.aws_security_group.bastion"
   }
 
 }
@@ -234,8 +234,8 @@ resource "aws_security_group" "bastion" {
 * The public key for the key pair we'll use to ssh into our bastion instance.
 */
 resource "aws_key_pair" "bastion" {
-  key_name = "ceros-ski-bastion-key-us-east-1a"
-  public_key = file(var.public_key_path) 
+  key_name    = "ceros-ski-bastion-key-us-east-1a"
+  public_key  = file(var.public_key_path) 
 }
 
 /**
@@ -251,19 +251,19 @@ data "aws_ssm_parameter" "linux2_ami" {
 * this availabilty zone.
 */
 resource "aws_instance" "bastion" {
-  ami = data.aws_ssm_parameter.linux2_ami.value
-  key_name = aws_key_pair.bastion.key_name 
-  instance_type = "t3.micro"
+  ami                         = data.aws_ssm_parameter.linux2_ami.value
+  key_name                    = aws_key_pair.bastion.key_name 
+  instance_type               = "t3.micro"
 
   associate_public_ip_address = true
-  subnet_id = element(aws_subnet.public_subnets, 0).id
-  vpc_security_group_ids = [aws_security_group.bastion.id]
+  subnet_id                   = element(aws_subnet.public_subnets, 0).id
+  vpc_security_group_ids      = [aws_security_group.bastion.id]
 
   tags = {
-    Application = "ceros-ski" 
-    Environment = var.environment 
-    Name = "ceros-ski-${var.environment}-us-east-1a-bastion"
-    Resource = "modules.availability_zone.aws_instance.bastion"
+    Application               = "ceros-ski" 
+    Environment               = var.environment 
+    Name                      = "ceros-ski-${var.environment}-us-east-1a-bastion"
+    Resource                  = "modules.availability_zone.aws_instance.bastion"
   }
 }
 
@@ -283,12 +283,12 @@ resource "aws_instance" "bastion" {
 */
 
 resource "aws_ecs_cluster" "cluster" {
-  name = "ceros-ski-${var.environment}"
+  name          = "ceros-ski-${var.environment}"
 
   tags = {
     Application = "ceros-ski"
     Environment = var.environment
-    Resource = "modules.ecs.cluster.aws_ecs_cluster.cluster"
+    Resource    = "modules.ecs.cluster.aws_ecs_cluster.cluster"
   }
 }
 
@@ -335,11 +335,11 @@ data "aws_iam_policy_document" "ecs_agent" {
 * The policy resource itself.  Uses the policy document defined above.
 */
 resource "aws_iam_policy" "ecs_agent" {
-  name = "ceros-ski-ecs-agent-policy"
-  path = "/"
+  name        = "ceros-ski-ecs-agent-policy"
+  path        = "/"
   description = "Access policy for the EC2 instances backing the ECS cluster."
 
-  policy = data.aws_iam_policy_document.ecs_agent.json
+  policy      = data.aws_iam_policy_document.ecs_agent.json
 }
 
 /**
@@ -348,11 +348,11 @@ resource "aws_iam_policy" "ecs_agent" {
 */
 data "aws_iam_policy_document" "ecs_agent_assume_role_policy" {
   statement {
-    actions = [
-      "sts:AssumeRole"
+    actions       = [
+                    "sts:AssumeRole"
     ]
     principals {
-      type = "Service"
+      type        = "Service"
       identifiers = ["ec2.amazonaws.com"]
     }
   }
@@ -363,8 +363,8 @@ data "aws_iam_policy_document" "ecs_agent_assume_role_policy" {
 * The IAM role that will be used by the instances that back the ECS Cluster.
 */
 resource "aws_iam_role" "ecs_agent" {
-  name = "ceros-ski-ecs-agent"
-  path = "/"
+  name               = "ceros-ski-ecs-agent"
+  path               = "/"
 
   assume_role_policy = data.aws_iam_policy_document.ecs_agent_assume_role_policy.json
 }
@@ -374,7 +374,7 @@ resource "aws_iam_role" "ecs_agent" {
 * above in the role itself.
 */
 resource "aws_iam_role_policy_attachment" "ecs_agent" {
-  role = aws_iam_role.ecs_agent.name 
+  role       = aws_iam_role.ecs_agent.name 
   policy_arn = aws_iam_policy.ecs_agent.arn 
 }
 
@@ -394,32 +394,32 @@ resource "aws_iam_instance_profile" "ecs_agent" {
 * instances to be deregistered.
 */
 resource "aws_security_group" "autoscaling_group" {
-  name        = "ceros-ski-${var.environment}-autoscaling_group"
-  description = "Security Group for the Autoscaling group which provides the instances for the ECS Cluster."
-  vpc_id      = aws_vpc.main_vpc.id 
+  name              = "ceros-ski-${var.environment}-autoscaling_group"
+  description       = "Security Group for the Autoscaling group which provides the instances for the ECS Cluster."
+  vpc_id            = aws_vpc.main_vpc.id 
 
   ingress {
-    description = "HTTP Ingress"
-    from_port   = 80 
-    to_port     = 80 
-    protocol    = "tcp"
+    description     = "HTTP Ingress"
+    from_port       = 80 
+    to_port         = 80 
+    protocol        = "tcp"
     security_groups = [aws_security_group.lb.id]
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
   }
-  depends_on = [
-    aws_security_group.lb
+  depends_on        = [
+                      aws_security_group.lb
   ]
 
   tags = {
-    Application = "ceros-ski"
-    Environment = var.environment
-    Resource = "modules.ecs.cluster.aws_security_group.autoscaling_group"
+    Application     = "ceros-ski"
+    Environment     = var.environment
+    Resource        = "modules.ecs.cluster.aws_security_group.autoscaling_group"
   }
 }
 
@@ -428,6 +428,8 @@ resource "aws_security_group" "autoscaling_group" {
 * Linux 2 maintained by AWS.  We'll use it to launch the instances that back
 * our ECS cluster.
 */
+
+
 data "aws_ssm_parameter" "cluster_ami_id" {
   name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended/image_id"
 }
@@ -436,11 +438,11 @@ data "aws_ssm_parameter" "cluster_ami_id" {
 * The launch configuration for the autoscaling group that backs our cluster.  
 */
 resource "aws_launch_configuration" "cluster_laucher" {
-  name = "ceros-ski-${var.environment}-cluster"
-  image_id = data.aws_ssm_parameter.cluster_ami_id.value 
-  instance_type = "t3.micro"
+  name                 = "ceros-ski-${var.environment}-cluster"
+  image_id             = data.aws_ssm_parameter.cluster_ami_id.value 
+  instance_type        = "t3.micro"
   iam_instance_profile = aws_iam_instance_profile.ecs_agent.name
-  security_groups = [aws_security_group.autoscaling_group.id]
+  security_groups      = [aws_security_group.autoscaling_group.id]
 
   // Register our EC2 instances with the correct ECS cluster.
   user_data = <<EOF
@@ -453,26 +455,26 @@ EOF
 * The autoscaling group that backs our ECS cluster.
 */
 resource "aws_autoscaling_group" "cluster" {
-  name = "ceros-ski-${var.environment}-cluster"
-  min_size = 1
-  max_size = 2 
+  name                 = "ceros-ski-${var.environment}-cluster"
+  min_size             = 1
+  max_size             = 2 
   
-  vpc_zone_identifier = [for subnet in aws_subnet.private_subnets : subnet.id] 
+  vpc_zone_identifier  = [for subnet in aws_subnet.private_subnets : subnet.id] 
   launch_configuration = aws_launch_configuration.cluster_laucher.name 
 
   tags = [{
-    "key" = "Application"
-    "value" = "ceros-ski"
+    "key"                 = "Application"
+    "value"               = "ceros-ski"
     "propagate_at_launch" = true
   },
   {
-    "key" = "Environment"
-    "value" = var.environment
+    "key"                 = "Environment"
+    "value"               = var.environment
     "propagate_at_launch" = true
   },
   {
-    "key" = "Resource"
-    "value" = "modules.ecs.cluster.aws_autoscaling_group.cluster"
+    "key"                 = "Resource"
+    "value"               = "modules.ecs.cluster.aws_autoscaling_group.cluster"
     "propagate_at_launch" = true
   }]
 }
@@ -482,8 +484,8 @@ resource "aws_autoscaling_group" "cluster" {
 * wrapper around the container definition.
 */
 resource "aws_ecs_task_definition" "backend" {
-  family = "ceros-ski-${var.environment}-backend"
-  network_mode = "awsvpc"
+  family                = "ceros-ski-${var.environment}-backend"
+  network_mode          = "awsvpc"
 
   container_definitions = <<EOF
 [
@@ -513,8 +515,8 @@ EOF
   tags = {
     Application = "ceros-ski" 
     Environment = var.environment 
-    Name = "ceros-ski-${var.environment}-backend"
-    Resource = "modules.environment.aws_ecs_task_definition.backend"
+    Name        = "ceros-ski-${var.environment}-backend"
+    Resource    = "modules.environment.aws_ecs_task_definition.backend"
   }
 }
 
@@ -533,78 +535,78 @@ EOF
 * constraints on the tasks.
 */
 resource "aws_ecs_service" "backend" {
-  name = "ceros-ski-${var.environment}-backend"
-  cluster = aws_ecs_cluster.cluster.id 
+  name            = "ceros-ski-${var.environment}-backend"
+  cluster         = aws_ecs_cluster.cluster.id 
   task_definition = aws_ecs_task_definition.backend.arn
   
-  desired_count = 2 
-  deployment_minimum_healthy_percent = 50
-  deployment_maximum_percent = 100
+  desired_count                       = 2 
+  deployment_minimum_healthy_percent  = 50
+  deployment_maximum_percent          = 100
 
   network_configuration {
-    security_groups = [aws_security_group.autoscaling_group.id]
-    subnets = aws_subnet.private_subnets.*.id
+    security_groups   = [aws_security_group.autoscaling_group.id]
+    subnets           = aws_subnet.private_subnets.*.id
   }
   load_balancer {
-    target_group_arn = aws_lb_target_group.ceros-ski.id
-    container_name = "ceros-ski"
-    container_port = 80
+    target_group_arn  = aws_lb_target_group.ceros-ski.id
+    container_name    = "ceros-ski"
+    container_port    = 80
   }
-  depends_on = [
-    aws_lb_listener.ceros-ski
+  depends_on          = [
+                        aws_lb_listener.ceros-ski
   ]
  
   ordered_placement_strategy {
-    type  = "binpack"
-    field = "cpu"
+    type              = "binpack"
+    field             = "cpu"
   }
 
   tags = {
-    Application = "ceros-ski" 
-    Environment = var.environment 
-    Resource = "modules.environment.aws_ecs_service.backend"
+    Application       = "ceros-ski" 
+    Environment       = var.environment 
+    Resource          = "modules.environment.aws_ecs_service.backend"
   }
 }
 
 resource "aws_lb" "default" {
-  name = "ecs-lb"
-  subnets = aws_subnet.public_subnets.*.id
+  name            = "ecs-lb"
+  subnets         = aws_subnet.public_subnets.*.id
   security_groups = [aws_security_group.lb.id]
 }
 
 resource "aws_lb_target_group" "ceros-ski" {
-  name = "ceros-ski-target"
-  port = 80
-  protocol = "HTTP"
-  vpc_id = aws_vpc.main_vpc.id
+  name        = "ceros-ski-target"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main_vpc.id
   target_type = "ip"
 }
 
 resource "aws_lb_listener" "ceros-ski" {
   load_balancer_arn = aws_lb.default.id
-  port = "80"
-  protocol = "HTTP"
+  port              = "80"
+  protocol          = "HTTP"
 
   default_action {
     target_group_arn = aws_lb_target_group.ceros-ski.id
-    type = "forward"
+    type             = "forward"
   }
 }
 
 resource "aws_security_group" "lb" {
-  name = "ecs-alb-security-group"
-  vpc_id = aws_vpc.main_vpc.id
+  name          = "ecs-alb-security-group"
+  vpc_id        = aws_vpc.main_vpc.id
 
   ingress {
-    protocol = "tcp"
-    from_port = 80
-    to_port = 80
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
